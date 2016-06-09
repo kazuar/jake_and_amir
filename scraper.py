08f1f964f22858da185fcd2457bb5bd7fd3902e5
Tzahi Vidas <tzahi.vidas@gmail.com>
2016-06-08 22:07:53 -0400

    Scrape main page

--- scraper.py
+++ scraper.py
@@ -1,10 +1,24 @@
 
 import requests
+from bs4 import BeautifulSoup
 
-URL = "http://scripts.jakeandamir.com/"
+BASE_URL = "http://scripts.jakeandamir.com/"
+URL_PARAMS = "index.php?search=a&from-date=&to-date=&do-search=1"
+
+def get_details(article):
+    return None
 
 def main():
-    result = requests.get(URL)
+    url = BASE_URL + URL_PARAMS
+    result = requests.get(url)
+    soup = BeautifulSoup(result.text, "html.parser")
+
+    articles = soup.find_all("article")
+
+    print "Found {0} articles".format(articles)
+
+    for article in articles:
+        details = get_details(article)
 
 if __name__ == '__main__':
     main()
